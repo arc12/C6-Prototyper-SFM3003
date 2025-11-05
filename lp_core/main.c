66db16ef82973bb19db9eadee30c6f208e555f25
Adam Cooper <arc@hilltop-cottage.info>
2025-11-05 12:08:12 +0000

    improve SFM interactions

--- lp_core/main.c
+++ lp_core/main.c
@@ -25,10 +25,8 @@ const uint8_t sleep_cmd[2] = {0x36, 0x77};
 
 int main (void)
 {
-    // these two are here to prevent the compiler optimising-out thes variables, which are really for book-keeping use from HP Core.
-    // beware - the test for loaded does not force a reload if the LP Core code changed; need to hard reset when developing changes to LP Core code
+    // for book-keeping use from HP Core.
     is_started = 1;
-    // loaded_code = 0xADC0;
 
     // uint8_t data_wr = 0;
     uint8_t data_rd[2];
@@ -41,70 +39,80 @@ int main (void)
     // otherwise we can get stuck with it in the wrong state and exiting without cleaning up
 
     // wake SFM3003 from sleep - wake-up requires a valid I2C address with the R/W bit low (write).
-    // lp_core_printf("LP CORE: wake sfm\n");
-    lp_core_i2c_master_set_ack_check_en(LP_I2C_NUM_0, false);  // disable ACK check because there wont be one
-    err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, cmd_idle, 2, 1);  // nominal wait - this is inserted between each byte
-    if ((err != ESP_OK) && (err != ESP_ERR_TIMEOUT)){  // ESP_ERR_TIMEOUT is the expected state if device was in sleep.
+    // lp_core_i2c_master_set_ack_check_en(LP_I2C_NUM_0, false);  // disable ACK check because there wont be one - this gives timeout and ESP_ERR_TIMEOUT. Otherwise NACK -> ESP_ERR_INVALID_RESPONSE
+    err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, cmd_idle, 2, 500);  // nominal wait - this is inserted between each byte
+    if ((err != ESP_OK) && (err != ESP_ERR_INVALID_RESPONSE)){  // ESP_ERR_INVALID_RESPONSE is the expected state if device was in sleep.
         last_err = (uint32_t)err;
         err_step = 1;
         goto exit_point;
     }
         
-    // the doc says wakeup should take about 16ms but it also says the sensor should be polled.
-    // Was 18000us but get ESP_ERR_INVALID_RESPONSE 
-    ulp_lp_core_delay_us(18000);  // TODO replace with delay + a poll by repeating the next write a few times if not ESP_OK (expect ESP_ERR_TIMEOUT?)
+    // the doc says wakeup should take about 16ms but it also says the sensor should be polled, so delay 15ms then poll
+    ulp_lp_core_delay_us(15000);
 
     // put into measurement mode. See the HP sfm3003.c for notes
     lp_core_i2c_master_set_ack_check_en(LP_I2C_NUM_0, true);  // restore ACK check
     // last param is "ticks_to_wait", not ms. Not clear what a tick is on LP core but I think it means 1 CPU clock - see LP_CORE_CPU_FREQUENCY_HZ
     // so 1 tick = 1/16 us for 16MHz clock., 15000 = 937.5us
     #define I2C_WAIT_TICKS 15000UL
-    // lp_core_printf("LP CORE: to measurement mode\n");
-    err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, cmd_measure, 2, I2C_WAIT_TICKS);
+    uint8_t try_cnt = 10;
+    while (--try_cnt > 0){
+        err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, cmd_measure, 2, I2C_WAIT_TICKS);
+        if ((err != ESP_ERR_TIMEOUT) && (err != ESP_ERR_INVALID_RESPONSE))break;  // I think it will be ESP_ERR_INVALID_RESPONSE if not ready
+    }
     if (err != ESP_OK){
         last_err = (uint32_t)err;
         err_step = 2;
         goto shutdown;
     }
     // and take readings
-    // TODO consider further delay before reading averaged flow - see sfm_read_oneshot() in sfm3003.c
+    // // total warmup of 30ms given in datasheet. See sfm_read_oneshot() in sfm3003.c for delay and double read rationale
     uint8_t result[6];  // 2 bytes flow + CRC + 2 bytes temp + CRC
-    ulp_lp_core_delay_us(30000);  // total warmup of 30ms given in datasheet
-    // lp_core_printf("LP CORE: read result\n");
-    err = lp_core_i2c_master_read_from_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, result, sizeof(result), I2C_WAIT_TICKS);
+    uint8_t result_t[6];  // used to get pre-warm temp
+    ulp_lp_core_delay_us(12000);
+    err = lp_core_i2c_master_read_from_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, result_t, sizeof(result_t), I2C_WAIT_TICKS);
     if (err != ESP_OK){
         last_err = (uint32_t)err;
         err_step = 3;
         goto shutdown;
     }
-    
+    ulp_lp_core_delay_us(18000);
+    // clear average from warm-up period
+    lp_core_i2c_master_read_from_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, result, sizeof(result), I2C_WAIT_TICKS);
+    // wait to average over about 60 readings
+    ulp_lp_core_delay_us(30000);
+    err = lp_core_i2c_master_read_from_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, result, sizeof(result), I2C_WAIT_TICKS);
+    if (err != ESP_OK){
+        last_err = (uint32_t)err;
+        err_step = 4;
+        goto shutdown;
+    }
+
     // transfer raw bytes to buffer (conversion done in HP Core) and inc pointer. Explicit loop rather than memcpy because of type difference
-    for (uint8_t i = 0; i < 6; i++){
+    for (uint8_t i = 0; i < 3; i++){  // flow
         raw_buffer[buffer_ix][i] =  (uint32_t)result[i];
+    }
+    for (uint8_t i = 3; i < 6; i++){  // temp
+        raw_buffer[buffer_ix][i] =  (uint32_t)result_t[i];
     }
     buffer_ix = ++buffer_ix % CONFIG_SFM_LP_BUFF_LEN;
     if (buffer_valid < CONFIG_SFM_LP_BUFF_LEN) ++buffer_valid;
 
-    // lp_core_printf("LP CORE: after storing, ix=%u, valid=%u\n", buffer_ix, buffer_valid);
-
     // put SFM3003 to sleep for low current. First need to put into idle mode.
-    // lp_core_printf("LP CORE: sfm to idle\n");
 shutdown:
-    err_step = 4;
     err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, cmd_idle, 2, I2C_WAIT_TICKS);
-    if ((err != ESP_OK) && (last_err != ESP_OK)){
+    if ((err != ESP_OK) && (last_err == ESP_OK)){
         last_err = (uint32_t)err;  // but still continue to attempt a sleep
-        err_step = 3;
+        err_step = 5;
     }
     
 exit_point:
     // Includes 0.5ms delay needed for it to be receptive to further commands after going idle
     ulp_lp_core_delay_us(500);
-    // lp_core_printf("LP CORE: sfm to sleep\n");
     err = lp_core_i2c_master_write_to_device(LP_I2C_NUM_0, SFM3003_7BIT_ADDR, sleep_cmd, 2, I2C_WAIT_TICKS);
-    if ((err != ESP_OK) && (last_err != ESP_OK)){
+    if ((err != ESP_OK) && (last_err == ESP_OK)){
         last_err = (uint32_t)err;
-        err_step = 5;
+        err_step = 6;
     }
 
     working_flag = 0;
